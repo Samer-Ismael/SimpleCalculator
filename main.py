22fffc93e6523a3d92175d50d0c50405108737f1
Samer Ismael <samerismaeelbat2@hotmail.com>
2023-03-14 21:29:00 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+print("Welcome to calculator")
+while True:
+    first = input("Enter first number: ")
+    operation = input("Enter operation: ")
+    second = input("Enter second number: ")
+
+    if operation == "+":
+        print(float(first) + float(second))
+    if operation == "-":
+        print(float(first) - float(second))
+    if operation == "*":
+        print(float(first) * float(second))
+    if operation == "/":
+        print(float(first) / float(second))
+    print("------------")
+
+    loop = input("Do you want to continue? Y/N")
+    print("------------")
+    if str(loop).lower() == "y":
+        break
+
